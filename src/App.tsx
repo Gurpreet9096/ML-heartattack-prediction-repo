c4b578b873882cc8c7c4cff151392f65b42bb53e
Gurpreet9096 <gurpreet.kour9096@gmail.com>
2025-05-28 19:05:26 +0530

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,26 @@
+import React from 'react';
+import { NavigationProvider } from './context/NavigationContext';
+import Layout from './components/Layout/Layout';
+import HeroSection from './components/Home/HeroSection';
+import Dashboard from './components/Dashboard/Dashboard';
+import PredictionForm from './components/Prediction/PredictionForm';
+import InfoSection from './components/Home/InfoSection';
+import AboutSection from './components/About/AboutSection';
 
+import './App.css';
+
+function App() {
+  return (
+    <NavigationProvider>
+      <Layout>
+        <HeroSection />
+        <Dashboard />
+        <PredictionForm />
+        <InfoSection />
+        <AboutSection />
+      </Layout>
+    </NavigationProvider>
+  );
+}
+
+export default App;
